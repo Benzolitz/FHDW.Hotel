21ea542f3d4c655004042d15a9931562984938b2
Lucas Engel <engellucas@gmx.de>
2015-10-23 11:47:44 +0200

    .... - added BaseRepository

---
+++ Repository/BaseRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace FHDW.Hotel.Repository
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class BaseRepository
+    {
+        private const string connectionString = @"server=localhost;userid=root;password=root;database=fhdw.hotel";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="p_query"></param>
+        /// <returns></returns>
+        public object ExecuteQuery(string p_query)
+        {
+            try
+            {
+                using (MySqlConnection con = new MySqlConnection(connectionString ))
+                {
+                    using (MySqlCommand com = new MySqlCommand(p_query, con))
+                    {
+                        return com.ExecuteNonQuery();
+                    }   
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
